273d6c97236c368f01be4a556db67ec9d0c027b5
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-02-03 21:58:30 +0000

    Add potential solution for characters to string exercise

---
+++ exercise_set/ruby/characters_to_string_solution.rb
@@ -0,0 +1,19 @@
+def head(list)
+  list[0]
+end
+
+def tail(list)
+  list[1..]
+end
+
+def make_string(list)
+  if list.size == 1
+    return list[0]
+  else
+    head(list) + make_string(tail(list))
+  end
+end
+
+pet = ['T','i','n','y', ' ', 'C', 'a', 't']
+puts make_string(pet) == "Tiny Cat" # test for correct return
+puts pet == ['T','i','n','y', ' ', 'C', 'a', 't'] # test that pet array has not been mutated
