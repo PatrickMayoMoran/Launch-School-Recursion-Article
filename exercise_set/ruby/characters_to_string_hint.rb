455aabd5d94450000ac57738a21373a292d4e68c
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-02-03 21:43:36 +0000

    Add hint for characters to string

---
+++ exercise_set/ruby/characters_to_string_hint.rb
@@ -0,0 +1,10 @@
+# HINT: Our base case would be a list with only a single character, like
+one_char = ['T']
+
+# If this were your input, what would you return?
+# How can you recursively call make_string() with an argument that gets
+# closer and closer to a list with only a single character?
+
+pet = ['T','i','n','y', ' ', 'C', 'a', 't']
+puts make_string(pet) == "Tiny Cat" # test for correct return
+puts pet == ['T','i','n','y', ' ', 'C', 'a', 't'] # test that pet array has not been mutated
