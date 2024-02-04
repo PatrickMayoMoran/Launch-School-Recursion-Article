8a2bcdd3232af624afe01a9e5351fd866b26ec80
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-02-04 17:03:45 +0000

    Remove unneeded comment

--- exercise_set/ruby/count_the_animal_hint.rb
+++ exercise_set/ruby/count_the_animal_hint.rb
@@ -4,7 +4,7 @@ count_the_animal([], 'cat')
 # When it's not our base case, we need some branching logic -
 #   - What to do when there's a match
 #   - What to do when there's not a match
-#
+
 # Both the above branches in our logic will need a recursive call
 
 def count_the_animal(animals, animal)
