455aea8f811bb9b7b5b9bdfed4f6ea2e86ed6c58
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-02-04 16:54:20 +0000

    Correct method name

--- exercise_set/ruby/count_the_animal_solution.rb
+++ exercise_set/ruby/count_the_animal_solution.rb
@@ -1,13 +1,13 @@
 # Include head and tail helper methods
 require_relative 'helper_methods'
 
-def count_the_animals(animals, animal)
+def count_the_animal(animals, animal)
   if animals.empty?
     0
   elsif head(animals) == animal
-    1 + count_the_animals(tail(animals), animal)
+    1 + count_the_animal(tail(animals), animal)
   else
-    count_the_animals(tail(animals), animal)
+    count_the_animal(tail(animals), animal)
   end
 end
 
@@ -22,12 +22,12 @@ animals = [
           ]
 
 # Test cases
-puts count_the_animals([], 'cat') == 0
-puts count_the_animals(animals, 'cat') == 12
-puts count_the_animals(animals, 'dog') == 4
-puts count_the_animals(animals, 'narwhal') == 1
-puts count_the_animals(animals, 'gecko') == 5
-puts count_the_animals(animals, 'llama') == 0
+puts count_the_animal([], 'cat') == 0
+puts count_the_animal(animals, 'cat') == 12
+puts count_the_animal(animals, 'dog') == 4
+puts count_the_animal(animals, 'narwhal') == 1
+puts count_the_animal(animals, 'gecko') == 5
+puts count_the_animal(animals, 'llama') == 0
 puts animals == [
             'cat', 'dog', 'cat', 'cow', 'horse', 'lizard',
             'gecko', 'dog', 'cat', 'mouse', 'moose', 'moose',
