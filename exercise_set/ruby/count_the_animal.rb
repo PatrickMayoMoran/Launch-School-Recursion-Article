6ca91abcf92a955adee002b11b978e55722af8f8
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-02-04 16:31:40 +0000

    Add problem prompt for count_the_animal problem

---
+++ exercise_set/ruby/count_the_animal.rb
@@ -0,0 +1,58 @@
+# Write a method, count_the_animal, that takes in a list of animals
+# and a single animal and returns a count of how many of that
+# animal are in the list. You can assume the input will always be a list
+# with at least 1 animal and that a given animal to look for
+# will always be provided in the form of a string.
+# (i.e. no need to check for input type or type of elements)
+#
+# Try to write a recursive solution! A recursive solution includes:
+#   1. Checking for the base case and returning a value
+#   2. If the base case is not met, taking a step that moves
+#      towards the base case
+#   3. Calling the method
+#
+# Recursive solutions also - ideally - do not mutate the given inputs.
+#
+#   The three steps above don't necessarily come in that order or each
+#   occupy their own line - some might share lines, you could write the
+#   order differently, etc.
+#
+# How to figure out the base case?
+#   A base case can be thought as the simplest version of the given 
+#   input. In this case, the input is a list of animals. What might
+#   the simplest version of this input be?
+#
+# Feel free to write your own test cases in addition to the ones below.
+#
+# If you're stumped, check out the count_the_animal_hint.rb file
+# A sample solution is provided in count_the_animal_solution.rb
+
+animals = [
+            'cat', 'dog', 'cat', 'cow', 'horse', 'lizard',
+            'gecko', 'dog', 'cat', 'mouse', 'moose', 'moose',
+            'bear', 'horse', 'cat', 'lizard', 'gecko', 'lizard',
+            'lizard', 'gecko', 'cat', 'cat', 'bear', 'cat',
+            'gecko', 'dog', 'cat', 'mouse', 'moose', 'moose',
+            'lizard', 'gecko', 'cat', 'narwhal', 'bear', 'cat',
+            'cat', 'dog', 'cat', 'cow', 'horse', 'lizard'
+          ]
+
+# Include head and tail helper methods
+require_relative 'helper_methods'
+
+# Your recursive method definition here
+
+# Test cases
+puts count_the_animal(animals, 'cat') 
+puts count_the_animal(animals, 'dog')
+puts count_the_animal(animals, 'narwhwal')
+puts count_the_animal(animals, 'gecko')
+puts animals == [
+            'cat', 'dog', 'cat', 'cow', 'horse', 'lizard',
+            'gecko', 'dog', 'cat', 'mouse', 'moose', 'moose',
+            'bear', 'horse', 'cat', 'lizard', 'gecko', 'lizard',
+            'lizard', 'gecko', 'cat', 'narwhal', 'bear', 'cat',
+            'gecko', 'dog', 'cat', 'mouse', 'moose', 'moose',
+            'lizard', 'gecko', 'cat', 'narwhal', 'bear', 'cat',
+            'cat', 'dog', 'cat', 'cow', 'horse', 'lizard'
+          ] # Check for non-mutated input array
