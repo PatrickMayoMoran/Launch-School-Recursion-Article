79187911dbe3e074b56ee6ebac5511cfffd7033d
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-02-04 16:59:03 +0000

    Add hint file for count_the_animal problem

---
+++ exercise_set/ruby/count_the_animal_hint.rb
@@ -0,0 +1,18 @@
+# Our base case is an empty array; what should you return?
+count_the_animal([], 'cat')
+
+# When it's not our base case, we need some branching logic -
+#   - What to do when there's a match
+#   - What to do when there's not a match
+#
+# Both the above branches in our logic will need a recursive call
+
+def count_the_animal(animals, animal)
+  if # base case check
+    # base case return
+  elsif # check for a match
+    # what to do when there's a match + recursive call
+  else
+    # what to do when there's not a match + recursive call
+  end
+end
